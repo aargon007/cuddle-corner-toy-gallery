b4020fdb6e0fe17afffec2e6c43f0ad515410237
md muhaiminul <muhai4959@gmail.com>
2023-05-19 18:06:56 +0600

    add errorpage component

--- src/pages/shared/ErrorPage.jsx
+++ src/pages/shared/ErrorPage.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { useNavigate, useRouteError } from 'react-router-dom';
+import errorImage from '../../assets/404error.jpg'
 
 const ErrorPage = () => {
+    const navigate = useNavigate();
+
+    const goBack = () => {
+        navigate(-1);
+    };
+
     return (
-        <div>
-            
+        <div className='flex flex-col items-center h-screen w-screen'>
+            <img src={errorImage} alt="" className='max-w-md' />
+            <h1 className='md:text-3xl text-2xl font-mono uppercase text-center text-red-700 font-semibold px-5 mb-10'>
+                page not found
+            </h1>
+            <button onClick={goBack} className="px-8 py-4 text-large font-semibold rounded bg-purple-600 text-gray-50 hover:text-gray-200">Back to homepage</button>
         </div>
     );
 };
